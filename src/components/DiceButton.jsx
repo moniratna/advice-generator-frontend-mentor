f2865b68dc24355650d0b093b75c57fb0c45b615
moniratna <moniratna.munshi123@gmail.com>
2023-07-01 21:33:38 +0530

    button hover fixed

--- src/components/DiceButton.jsx
+++ src/components/DiceButton.jsx
@@ -4,7 +4,7 @@ import dice from "../assets/icon-dice.svg";
 export default function DiceButton({ onClick }) {
 	return (
 		<div
-			className=" shadow absolute flex hover:shadow-lg hover:shadow-[#52FFA8] hover:w-14 hover:h-14 hover:bottom-[-25px] justify-center items-center bg-[#52FFA8] rounded-full w-12 h-12 bottom-[-25px] cursor-pointer"
+			className=" shadow absolute flex hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)] justify-center items-center bg-[#52FFA8] rounded-full w-12 h-12 bottom-[-25px] cursor-pointer"
 			onClick={onClick}
 		>
 			<img src={dice} className="justify-center items-center" />
